b9d4e7e2823bab5bbce25a76a97230123a2bbd44
alice T <alicewtien@gmail.com>
2018-09-03 21:54:54 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -2,7 +2,11 @@ def hello_t(array)
   i = 0
 
   while i < array.length
-    yield array[i]
+    yield array[i] do |name|
+      if name.start_with?("T")
+        puts "Hi, #{name}"
+      end
+    end
     i = i + 1
   end
   array
